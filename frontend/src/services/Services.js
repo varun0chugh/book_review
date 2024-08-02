e737741c3c47908f0568f85b24df9f97d680ac2c
Ahmed hanzala <134609073+hanzalagithub@users.noreply.github.com>
2024-08-02 18:05:42 +0500

    Update Services.js

--- frontend/src/services/Services.js
+++ frontend/src/services/Services.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export const LoginHandler = async ({ email, password }) => {
 
-        const resp = await axios.post('http://localhost:4000/api/auth/login', { email, password });
+        const resp = await axios.post('https://book-review-platform-api.vercel.app/api/auth/login', { email, password });
         return resp;
 };
 
 
 export const registerHandler = async ({email, password, fullName})=>{
 
-    const resp = await axios.post('http://localhost:4000/api/auth/register',{email,password,fullName});
+    const resp = await axios.post('https://book-review-platform-api.vercel.app/api/auth/register',{email,password,fullName});
     return resp;
 
 }
